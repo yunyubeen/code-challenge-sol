78879ef564f33442b6f703017dbec74b27fc2707
yunyubeen <129297756+yunyubeen@users.noreply.github.com>
2024-04-19 02:02:26 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.52 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    ret = []
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            ret.append(numbers[i] + numbers[j])
+            
+    ret = sorted(set(ret))
+    return ret
