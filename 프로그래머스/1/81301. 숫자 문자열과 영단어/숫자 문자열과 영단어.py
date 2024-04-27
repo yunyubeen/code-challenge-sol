f43ffdd3bf7f8e02655ac36affb20d1ca9185d5d
yunyubeen <129297756+yunyubeen@users.noreply.github.com>
2024-04-27 17:42:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,6 @@
 def solution(s):
     english = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    answer = 0
     
     for i, v in enumerate(english) :
         s = s.replace(english[i], num[i])
